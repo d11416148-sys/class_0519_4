ae2895d69887a3c58be490d7fbdbb7fc3b0294cc
d11416148-sys <d11416148@gs.takming.edu.tw>
2026-05-19 15:51:40 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Person
+{
+    public string GetName()
+    {
+        return "Person";
+    }
+}
+
+public class Student : Person
+{
+    public string GetName()
+    {
+        return "Student";
+    }
+
+}
+
+
+
+public abstract class Person1
+{
+    public abstract string GetName();
+}
+
+public class Student1 : Person1
+{
+    public override string GetName()
+    {
+        return "Student";
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise_0519_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            Console.WriteLine(s.GetName());
+            Person p = s;
+            Console.WriteLine(p.GetName());
+
+
+
+            Student1 s1 = new Student1();
+            Console.WriteLine(s1.GetName());
+            Person1 p1 = s1;
+            Console.WriteLine(p1.GetName());
+
+
+        }
+    }
+}
